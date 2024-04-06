c2583d296380a1f2366eed9f1d0bfe52a1413c25
Sam <asamadans@gmail.com>
2024-04-07 03:52:32 +0530

    changed build backend back to setuptools, fixed some typing issues

--- src/bitssh/prompt.py
+++ src/bitssh/prompt.py
@@ -4,12 +4,14 @@ from .utils import ConfigPathUtility
 from .ui import UI
 import os
 
+from inquirer import List
+
 
 class Prompt:
     @classmethod
     def ask_host_prompt(cls) -> None:
-        HOST = ConfigPathUtility.get_config_file_host_data()
-        questions = [
+        HOST: list[str] = ConfigPathUtility.get_config_file_host_data()
+        questions: list[List] = [
             inquirer.List(
                 name="host",
                 message="Select the Host Given in the Above List",
